2b24cbe05490bd32bc641d617ba15152c50447b9
jeff8014 <jeff8014@gmail.com>
2021-10-12 15:59:25 +0900

    initial commit

---
+++ src/main/java/com/choijeonghwan/ratemybeer/RateMyBeerApplication.java
@@ -0,0 +1,13 @@
+package com.choijeonghwan.ratemybeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMyBeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateMyBeerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choijeonghwan/ratemybeer/RateMyBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choijeonghwan.ratemybeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateMyBeerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
